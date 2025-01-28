93b1f8d69a396ef34cc4d535d35ad636215dca97
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-01-28 14:42:09 -0600

    Added Model and POST method

---
+++ src/main/java/com/courses/kafkacurse/productsmicroservice/rest/CreateProductRestModel.java
@@ -0,0 +1,36 @@
+package com.courses.kafkacurse.productsmicroservice.rest;
+
+import java.math.BigDecimal;
+
+public class CreateProductRestModel {
+
+    //Attributes
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    //Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}

a7441e2474e991065f8d0b5d98782041591042a9
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-01-28 15:50:09 -0600

    Asynchronous way to send a message example

--- src/main/java/com/courses/kafkacurse/productsmicroservice/service/ProductServiceImpl.java
+++ src/main/java/com/courses/kafkacurse/productsmicroservice/service/ProductServiceImpl.java
@@ -1,17 +1,20 @@
 package com.courses.kafkacurse.productsmicroservice.service;
 
 import com.courses.kafkacurse.productsmicroservice.controller.CreateProductRestModel;
-import com.courses.kafkacurse.productsmicroservice.controller.ProductController;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    //@Autowired
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
 
     public ProductServiceImpl(KafkaTemplate<String,ProductCreatedEvent> kafkaTemplate){
@@ -25,7 +28,16 @@ public class ProductServiceImpl implements ProductService {
         // TODO: Persist Product Details into a database table before publishing an Event
 
         ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,product.getTitle(),product.getPrice(),product.getQuantity());
-        kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+        CompletableFuture<SendResult<String,ProductCreatedEvent>> future = kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                LOGGER.error("Failed to send message: " + ex.getMessage());
+            }else{
+                LOGGER.info("Message sent successfully: " + result.getRecordMetadata());
+
+            }
+        });
         return productId;
     }
 }

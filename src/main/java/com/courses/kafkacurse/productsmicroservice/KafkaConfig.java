59401b1650c89c3989150616f0fdea63113f16d8
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-02-05 14:47:07 -0600

    Idempotent Producer in Bean Configuration

--- src/main/java/com/courses/kafkacurse/productsmicroservice/KafkaConfig.java
+++ src/main/java/com/courses/kafkacurse/productsmicroservice/KafkaConfig.java
@@ -39,6 +39,13 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
 
+    //Idempotent Producer
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private boolean idempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private Integer inFlightRequests;
+
 
     //Producer configurations
     Map<String, Object> producerConfigs () {
@@ -50,6 +57,10 @@ public class KafkaConfig {
         configs.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         configs.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         configs.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        //Idempotent Producer
+        configs.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        configs.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inFlightRequests);
+//        configs.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
         return configs;
     }
 
@@ -62,7 +73,6 @@ public class KafkaConfig {
     KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate(){
         return new KafkaTemplate<String, ProductCreatedEvent> (producerFactory());
     }
-
 
     @Bean
     NewTopic createTopic(){
